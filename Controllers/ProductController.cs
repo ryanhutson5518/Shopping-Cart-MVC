864d655f4570e7f919ba2c1ce2fecc494cb5bba4
Ryan Hutson <ryanhutson5518@gmail.com>
2021-01-22 16:35:55 -0600

    Code files

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using ecommerceMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ecommerceMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly List<ProductModel> _products = JsonConvert.DeserializeObject<List<ProductModel>>(System.IO.File.ReadAllText(@"Data\Data.json"));
+
+        public IActionResult Index()
+        {
+            return View(_products);
+        }
+
+        public IActionResult ViewProduct(ProductModel product)   // This action takes in the single product from index ^. I can then send it to another view and do whatever I would like with the data.
+        {
+            var obj = _products[product.ID - 1];
+            return View(obj);
+        }
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ecommerceMVC.Models
+{
+    public class ProductModel
+    {
+        public int ID { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public decimal Rating { get; set; }
+        public decimal Price { get; set; }
+        public bool OnSale { get; set; }
+        public decimal SalePrice { get; set; }
+        public bool FreeShipping { get; set; }
+        public decimal ShippingCost { get; set; }
+        public decimal DisplaySize { get; set; }
+        public int DiskSize { get; set; }
+        public bool DiskSizeGB { get; set; }
+        public bool DiskSSD { get; set; }
+        public int RAM { get; set; }
+        public string OperatingSystem { get; set; }
+        public bool InStock { get; set; }
+        public int NumInStock { get; set; }
+        public string LongDescription { get; set; }
+    }
+}
